57f9520877b9672bebfbe1e7942ac9b643c410c5
vgeorgescu0 <71309994+vgeorgescu0@users.noreply.github.com>
2021-04-09 23:22:49 -0700

    Added getAll and remove functions and styled index.html
    Added more to firebase.js for FireStore manipulation (can now add, remove, and see regular updates either after adding/removing or if you add on the FireStore browser, there is an update list button). Also added a stylesheet to make everything look better.

--- Project 1/scripts/firebase.js
+++ Project 1/scripts/firebase.js
@@ -19,7 +19,65 @@ db.settings({ timestampsInSnapshots: true }); // This is to reduce errors in con
 
 // Create selector for html elements
 const form = document.querySelector('#add-dog-form');
+const list = document.querySelector('#dog-list');
 
+// Listener to submit button from form, instead of default it performs add function
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    add(form.name.value, form.phone.value, form.location.value, form.img.value);
+    form.name.value = '';
+    form.phone.value = '';
+    form.location.value = '';
+    form.img.value = '';
+});
+
+function renderList(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let phone = document.createElement('span');
+    let location = document.createElement('span');
+    let img = document.createElement('span');
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    phone.textContent = doc.data().phone;
+    location.textContent = doc.data().location;
+    img.textContent = doc.data().img;
+    cross.textContent = 'x';
+
+    li.appendChild(name);
+    li.appendChild(phone);
+    li.appendChild(location);
+    li.appendChild(img);
+    li.appendChild(cross);
+
+    list.appendChild(li);
+
+    // deleting data
+    cross.addEventListener('click', (event) => {
+        if (confirm('Are you sure?')) {
+            let id = event.target.parentElement.getAttribute('data-id');
+            db.collection('dogs').doc(id).delete().then((snapshot) => {
+            getAll();
+        });
+        } else {
+            return false;
+        }
+    });
+}
+
+function getAll() {
+    'use strict';
+    document.getElementById("dog-list").innerHTML = '';
+    db.collection('dogs').get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            renderList(doc);
+        });
+    });
+}
+
+getAll();
 
 // Function to add dog to firestore using parameters from form below
 function add(name, phone, location, img) {
@@ -32,18 +90,9 @@ function add(name, phone, location, img) {
     })
     .then(() => {
         console.log("Document successfully written!");
+        getAll();
     })
     .catch((error) => {
         console.error("Error writing document: ", error);
     });
 }
-
-// Listener to submit button from form, instead of default it performs add function
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    add(form.name.value, form.phone.value, form.location.value, form.img.value);
-    form.name.value = '';
-    form.phone.value = '';
-    form.location.value = '';
-    form.img.value = '';
-});
